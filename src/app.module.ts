63a254615310bafc78bd96b4ef16ab596a6665e3
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-05-14 20:49:13 -0300

    build(general): Add postgree connection
    - (env) refactor port to server_port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvironmentConfig } from './config/environment.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(new EnvironmentConfig().getTypeOrmConfig()),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/environment.config.ts
@@ -0,0 +1,51 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export class EnvironmentConfig {
+  private getValue(key: string, throwOnUndefined = true): string {
+    const value = process.env[key];
+    if (!value && throwOnUndefined) {
+      throw new Error(`Missing env.${key}`);
+    }
+
+    return value;
+  }
+
+  public ensureValues(keys: string[]) {
+    keys.forEach((k) => this.getValue(k, true));
+    return this;
+  }
+
+  public isOnProduction() {
+    const mode = this.getValue('NODE_ENV', false);
+    return mode != 'dev';
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    this.ensureValues([
+      'POSTGRES_HOST',
+      'POSTGRES_PORT',
+      'POSTGRES_USER',
+      'POSTGRES_PASSWORD',
+      'POSTGRES_DATABASE',
+    ]);
+
+    return {
+      type: 'postgres',
+      host: this.getValue('POSTGRES_HOST'),
+      port: parseInt(this.getValue('POSTGRES_PORT')),
+      username: this.getValue('POSTGRES_USER'),
+      password: this.getValue('POSTGRES_PASSWORD'),
+      database: this.getValue('POSTGRES_DATABASE'),
+      synchronize: false,
+      migrationsTableName: 'migration',
+      migrations: ['dist/migration/*.{ts,js}'],
+      entities: ['dist/models/*entity.{ts,js}'],
+      cli: {
+        entitiesDir: 'src/models',
+        migrationsDir: 'src/migration',
+      },
+
+      ssl: this.isOnProduction(),
+    };
+  }
+}
