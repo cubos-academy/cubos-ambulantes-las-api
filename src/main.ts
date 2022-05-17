037c0d7a1a28d41f1408c7ef7797fef6d4da3eec
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-05-17 00:23:07 -0300

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
 import generateOrmConfig from './config/generate-ormconfig';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   generateOrmConfig();
+
+  const config = new DocumentBuilder()
+    .setTitle('las-api')
+    .setDescription(
+      'Api que deve ser integrada no aplicativo de licenciamento de ambulantes de salvador',
+    )
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
