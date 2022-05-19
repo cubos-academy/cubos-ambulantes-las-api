451d35a8f25488d88de60c3e5f2f7bddffdf5809
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-05-19 15:00:59 -0300

    refactor(accreditations): :art: improve line break format

--- src/accreditation/dto/update-accreditation.dto.ts
+++ src/accreditation/dto/update-accreditation.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAccreditationDto } from './create-accreditation.dto';
 
-export class UpdateAccreditationDto extends PartialType(CreateAccreditationDto) {}
+export class UpdateAccreditationDto extends PartialType(
+  CreateAccreditationDto,
+) {}
