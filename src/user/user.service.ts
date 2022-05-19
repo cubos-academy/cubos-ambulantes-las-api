42974e8a884136009fac956c632b8d5ca7627d5d
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-05-18 23:38:06 -0300

    refactor(user): :art: improve code readability

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,25 +11,26 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(UserEntity) private readonly repo: Repository<UserEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
 
     @InjectRepository(UserAddressEntity)
-    private readonly addressRepo: Repository<UserAddressEntity>,
+    private readonly addressRepository: Repository<UserAddressEntity>,
 
     @InjectRepository(UserContactsEntity)
-    private readonly contactsRepo: Repository<UserContactsEntity>,
+    private readonly contactsRepository: Repository<UserContactsEntity>,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
     //Address (Relational)
     const addressEntity = new UserAddressEntity();
-    const createAddress = await this.addressRepo.save(addressEntity);
+    const createAddress = await this.addressRepository.save(addressEntity);
     createUserDto.address = createAddress;
 
     //Contacts (Relational)
     const contactsEntity = new UserContactsEntity();
     contactsEntity.email = createUserDto.email;
-    const createContact = await this.contactsRepo.save(contactsEntity);
+    const createContact = await this.contactsRepository.save(contactsEntity);
     createUserDto.contacts = createContact;
 
     //User
@@ -38,34 +39,42 @@ export class UserService {
 
     createUserDto.createdAt = new Date();
 
-    const result = await this.repo.save(createUserDto);
-    delete result.password;
-    delete result.address.id;
-    delete result.contacts.id;
+    const result = await this.userRepository.save(createUserDto);
     delete result.email;
 
     return result;
   }
 
   async find(id: number): Promise<UserEntity> {
-    const result = await this.repo.findOne(
+    return this.userRepository.findOne(
       { id },
       { relations: ['address', 'contacts'] },
     );
-
-    if (result) {
-      delete result.password;
-      delete result.address.id;
-      delete result.contacts.id;
-    }
-
-    return result;
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
     updateUserDto.id = id;
-    await this.repo.save(updateUserDto);
+    await this.userRepository.save(updateUserDto);
 
-    return this.find(id);
+    return this.userRepository.findOne(id, {
+      relations: ['address', 'contacts'],
+    });
+  }
+
+  /**
+   * @description filter results from others methods and removes unnecessary columns
+   * @param entity User Entity
+   * @returns The same entity, but with no unnecessary columns
+   */
+  public filter(entity: UserEntity): UserEntity {
+    if (entity) {
+      delete entity.password;
+      delete entity.addressId;
+      delete entity.contactsId;
+      entity.address && delete entity.address.id;
+      entity.contacts && delete entity.contacts.id;
+    }
+
+    return entity;
   }
 }
