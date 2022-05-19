039d24ccc9b2f011104b43621d99b24c92e6ee77
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-05-19 17:25:44 -0300

    aaa

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -24,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
