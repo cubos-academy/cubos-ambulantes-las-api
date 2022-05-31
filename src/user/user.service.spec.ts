d1cbc0781401b2efa206ae61bbb1874018f71c40
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-05-30 21:45:50 -0300

    half work.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserAddressEntity } from '../user-address/entities/user_address.entity';
+import { UserContactsEntity } from '../user-contacts/entities/user_contacts.entity';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserEntity } from './entities/user.entity';
 import { UserService } from './user.service';
 
+const userEntityItem: UserEntity = new UserEntity({
+  id: 1,
+  fullName: 'Ivson Emidio',
+  cpf: '13938947472',
+  password: 'asdjsaidj2232',
+  contacts: new UserContactsEntity(),
+  address: new UserAddressEntity(),
+});
+
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            create: jest.fn().mockResolvedValue(userEntityItem),
+            save: jest.fn().mockResolvedValue(userEntityItem),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return a UserEntity successfully on creating user', async () => {
+      //set
+      const createUserDto = new CreateUserDto();
+      createUserDto.fullName = 'Ivson Emidio';
+      createUserDto.cpf = '13938947488';
+      createUserDto.email = 'contato@gmail.com';
+      createUserDto.password = '946788CA';
+
+      //act
+      const result = await userService.create(createUserDto);
+
+      //assert
+      expect(result).toBe(userEntityItem);
+    });
   });
 });
