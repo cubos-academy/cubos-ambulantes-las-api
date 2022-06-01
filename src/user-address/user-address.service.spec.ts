1904db64a52ba54df3e36f6d0a08a043afbe5034
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-06-01 12:01:32 -0300

    test(user-address): :white_check_mark: add missing userAddressService test

--- src/user-address/user-address.service.spec.ts
+++ src/user-address/user-address.service.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UpdateAddressDto } from './dto/update-user-address.dto';
+import { UserAddressEntity } from './entities/user_address.entity';
 import { UserAddressService } from './user-address.service';
 
+const userAddressEntityItem: UserAddressEntity = {
+  id: 1,
+  cep: '54420225',
+  city: 'Recife',
+  complement: 'APT 220',
+  district: 'Jaqueira',
+  number: 25,
+  state: 'PE',
+  street: 'Jaboatão dos Guararapes',
+};
+
 describe('UserAddressService', () => {
-  let service: UserAddressService;
+  let userAddressService: UserAddressService;
+  let userAddressRepo: Repository<UserAddressEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserAddressService],
+      providers: [
+        UserAddressService,
+        {
+          provide: getRepositoryToken(UserAddressEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(userAddressEntityItem),
+            save: jest.fn().mockResolvedValue(userAddressEntityItem),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserAddressService>(UserAddressService);
+    userAddressService = module.get<UserAddressService>(UserAddressService);
+    userAddressRepo = module.get<Repository<UserAddressEntity>>(
+      getRepositoryToken(UserAddressEntity),
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userAddressService).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(userAddressRepo).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return a user address entity', async () => {
+      //Arrange
+      const id = 1;
+
+      //Act
+      const result = await userAddressService.findOne(id);
+
+      //Assert
+      expect(result).toEqual(userAddressEntityItem);
+      expect(userAddressRepo.findOne).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should return a user address entity', async () => {
+      //Arrange
+      const id = 1;
+      const updateAddressDto = new UpdateAddressDto();
+      updateAddressDto.street = 'Rua Alan Kardec';
+
+      //Act
+      const result = await userAddressService.update(id, updateAddressDto);
+
+      //Assert
+      expect(result).toEqual(userAddressEntityItem);
+      expect(userAddressRepo.save).toHaveBeenCalledTimes(1);
+    });
   });
 });
